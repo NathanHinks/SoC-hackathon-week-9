2a6e60e022d97423d91b6c092cd5248c703a8d11
Nathan Hinks <nathanhinks.dev@gmail.com>
2021-04-26 19:11:04 +0100

    usePost error fixed

--- FrontEnd/src/hooks/useFetch.js
+++ FrontEnd/src/hooks/useFetch.js
@@ -1,24 +1,25 @@
 import { useEffect, useState } from "react";
 import { BACKEND_URL, getSong } from '../libs/dependencies';
 
+
 function useFetch(songLengthState) {
-  const [data, setData] = useState({});
+  const [data, setData] = useState(null);
 
   const PATH = getSong(songLengthState);
 
-  async function fetchData() {
-    let result = await fetch(`${BACKEND_URL}/songs/${PATH}`);
-    let songData = await result.json();
-
-    let quoteResult = await fetch(`${BACKEND_URL}/quotes/${PATH}`);
-    let quoteData = await quoteResult.json();
-    
-    setData({songData: songData, quoteData: quoteData});
-  }
-
   useEffect(() => {
+    async function fetchData() {
+      let result = await fetch(`${BACKEND_URL}/songs/${PATH}`);
+      let songData = await result.json();
+  
+      let quoteResult = await fetch(`${BACKEND_URL}/quotes/${PATH}`);
+      let quoteData = await quoteResult.json();
+      
+      setData({songData: songData, quoteData: quoteData});
+    }
+
     fetchData();
-  }, [songLengthState]);
+  }, [songLengthState, PATH]);
 
   return data;
 }
--- FrontEnd/src/hooks/usePost.js
+++ FrontEnd/src/hooks/usePost.js
@@ -2,18 +2,17 @@ import { useEffect } from "react";
 import { BACKEND_URL } from '../libs/dependencies';
 
 function usePost(newSong) {
-  function makePost() {
-    // our form returns object
-    // give object to post to add to db
-    const requestOptions = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(newSong),
-    };
-    fetch(`${BACKEND_URL}/songs`, requestOptions);
-  }
-
+  
   useEffect(() => {
+    function makePost() {
+      const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newSong),
+      };
+      fetch(`${BACKEND_URL}/songs`, requestOptions);
+    }
+
     makePost(newSong);
   }, [newSong]);
 }
