1df70c4e1db323b35d30ed4bc418010eacdf1810
Nathan Hinks <nathanhinks.dev@gmail.com>
2021-04-26 20:30:37 +0100

    form refactored slightly and songLengths moved out to dependencies

--- FrontEnd/src/Components/AddSongInput/index.js
+++ FrontEnd/src/Components/AddSongInput/index.js
@@ -1,82 +1,80 @@
 import { useState } from 'react';
 import usePost from '../../hooks/usePost';
+import { songLengths } from '../../libs/dependencies';
 import './AddSongInput.css';
 
 const initialData = {
-	Title          : 'blank',
-	Artist         : 'blank',
-	SongLengthCode : 'blank',
-	Link           : 'blank',
-	SuggestedBy    : 'blank',
+  Title: 'blank',
+  Artist: 'blank',
+  SongLengthCode: 'blank',
+  Link: 'blank',
+  SuggestedBy: 'blank',
 };
 
 function AddSongInput() {
-	const [ data, setData ] = useState(initialData);
-	const makePost= usePost();
+  const [data, setData] = useState(initialData);
+  const makePost = usePost();
 
-	function UpdateData(event) {
-		const key = event.target.id;
-		const newValue = event.target.value;
-		setData({ ...data, [key]: newValue });
-	}
+  function UpdateData(event) {
+    const key = event.target.id;
+    const newValue = event.target.value;
+	
+    setData({ ...data, [key]: newValue });
+  }
 
-	return (
-		<form id='form' className='add-song-form' action='submit'>
-			<input
-				type='text'
-				placeholder='Title'
-				id='Title'
-				onChange={(e) => UpdateData(e)}
-			/>
-			<input
-				type='text'
-				placeholder='Artist'
-				id='Artist'
-				onChange={(e) => UpdateData(e)}
-			/>
-			<input
-				type='text'
-				placeholder='Link'
-				id='Link'
-				onChange={(e) => UpdateData(e)}
-			/>
-			<input
-				type='text'
-				placeholder='Your Name'
-				id='SuggestedBy'
-				onChange={(e) => UpdateData(e)}
-			/>
-			<select
-				form='form'
-				type='text'
-				id='SongLengthCode'
-				onChange={(e) => UpdateData(e)}
-				required
-			>
-				<option value='0'>How Long is The Song?</option>
-				<option value='1'>0-60 seconds</option>
-				<option value='2'>61-90 seconds</option>
-				<option value='3'>91-120 seconds</option>
-				<option value='4'>121-150 seconds</option>
-				<option value='5'>151-180 seconds</option>
-				<option value='6'>181-210 seconds</option>
-				<option value='7'>211-240 seconds</option>
-				<option value='8'>241-280 seconds</option>
-			</select>
+  return (
+    <form id='form' className='add-song-form' action='submit'>
+      <input
+        type='text'
+        placeholder='Title'
+        id='Title'
+        onChange={UpdateData}
+      />
+      <input
+        type='text'
+        placeholder='Artist'
+        id='Artist'
+        onChange={UpdateData}
+      />
+      <input
+        type='text'
+        placeholder='Link'
+        id='Link'
+        onChange={UpdateData}
+      />
+      <input
+        type='text'
+        placeholder='Your Name'
+        id='SuggestedBy'
+        onChange={UpdateData}
+      />
+      <select
+        form='form'
+        type='text'
+        id='SongLengthCode'
+        onChange={UpdateData}
+        required
+      >
+        {songLengths.map((length) => (
+          <option
+            value={length.value}
+          >{`${length.minutes} (${length.seconds})`}</option>
+        ))}
+      </select>
 
-			<button
-				className='add-song-btn'
-				onClick={(event) => {
-					event.preventDefault();
-					makePost(data);
+      <button
+        className='add-song-btn'
+        onClick={(event) => {
+          event.preventDefault();
+          makePost(data);
 
-					document.querySelector('.add-song-form').reset();
-				}}
-			>
-				Add Song
-			</button>
-		</form>
-	);
+          document.querySelector('.add-song-form').reset();
+        }}
+      >
+        Add Song
+      </button>
+    </form>
+  );
 }
 
 export default AddSongInput;
--- FrontEnd/src/libs/dependencies.js
+++ FrontEnd/src/libs/dependencies.js
@@ -1,3 +1,51 @@
-export const BACKEND_URL = "https://supersquatsongapp.herokuapp.com"
+export const BACKEND_URL = 'https://supersquatsongapp.herokuapp.com';
 
-export const getSong = (songLengthState) => Math.max((songLengthState/30), 1);
+export const getSong = (songLengthState) => Math.max(songLengthState / 30, 1);
+
+export const songLengths = [
+  {
+    value: '0',
+    minutes: 'How Long is The Song?',
+    seconds: 'please select',
+  },
+  {
+    value: '1',
+    minutes: '0 - 1 minute',
+    seconds: '0s-60s',
+  },
+  {
+    value: '2',
+    minutes: '1 - 1.5 minutes',
+    seconds: '61s-90s',
+  },
+  {
+    value: '3',
+    minutes: '1.5 - 2 minutes',
+    seconds: '91s-120s',
+  },
+  {
+    value: '4',
+    minutes: '2 - 2.5 minutes',
+    seconds: '121s-150s',
+  },
+  {
+    value: '5',
+    minutes: '2.5 - 3 minutes',
+    seconds: '151s-180s',
+  },
+  {
+    value: '6',
+    minutes: '3 - 3.5 minutes',
+    seconds: '181s-210s',
+  },
+  {
+    value: '7',
+    minutes: '3.5 - 4 minutes',
+    seconds: '211s-240s',
+  },
+  {
+    value: '8',
+    minutes: '4 - 4.5 minutes',
+    seconds: '241s-280s',
+  },
+];
